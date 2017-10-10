4bf15a961bcc549de24aeae1ee10faf32fbb0281
Shane O Donovan <shaneod@live.ie>
2017-10-11 00:50:07 +0100

    Temp fix, allow redirect after login

--- index.js
+++ index.js
@@ -203,13 +203,13 @@ app.get('/settings', async (req, res) => {
 
 async function show_pug(pug_name, req, res) {
 	if (!req.session.logged_in) {
-		res.send(pug.renderFile(pug_name, { current_url: req.path, user: null, logged_in: false }))
+		res.send(pug.renderFile(pug_name, { current_url: req.path, next_url: req.params.next_url, user: null, logged_in: false }))
 		return
 	}
 	const user_model = mongoose.model(req.session.account_type)
 	try {
 		const user = await user_model.findOne({ _id: req.session.user_id }).lean().exec()
-		res.send(pug.renderFile(pug_name, { current_url: req.path, user: user, logged_in: true }))
+		res.send(pug.renderFile(pug_name, { current_url: req.path, next_url: req.params.next_url, user: user, logged_in: true }))
 	} catch (db_error) {
 		res.status(503).send("Failed to communicate with the database.")
 	}
